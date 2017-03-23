49d4e110ae4401696bbe3e3548cf27c50538ab7e
Jack Reed <phillipjreed@gmail.com>
2017-03-23 14:52:16 -0400

    add scaffolding for static home and about-iiif pages

---
+++ spec/routing/static_pages_routing_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Static page routing', type: :routing do
+  describe 'Home page' do
+    it { expect(get('/')).to route_to 'high_voltage/pages#show', id: 'home' }
+    it { expect(get('/home')).to route_to 'high_voltage/pages#show', id: 'home' }
+  end
+  describe 'about-iiif page' do
+    it { expect(get('/about-iiif')).to route_to 'high_voltage/pages#show', id: 'about-iiif' }
+  end
+end
