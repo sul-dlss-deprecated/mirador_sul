e5094182905fdc945674d9048457fe61dd873a8c
Darren Hardy <drh@stanford.edu>
2017-04-05 15:20:37 -0700

    temporarily switch to assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # TODO: switch to false when we figure out how to serve static assets from mirador_rails
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
