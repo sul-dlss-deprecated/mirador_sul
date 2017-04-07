25372618170a444c4358bff0e04d13492b2d44a9
Javier de la Rosa <versae@gmail.com>
2017-04-07 12:39:55 -0700

    Fixes #76. Adding collection description to collecton index

--- spec/views/collections/_collection.html.erb_spec.rb
+++ spec/views/collections/_collection.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'collections/_collection', type: :view do
-  let(:collection) { create(:collection) }
+  let(:collection) { create(:collection, description: 'description') }
   before do
     expect(view).to receive(:collection).at_least(:once).and_return(collection)
   end
@@ -16,6 +16,11 @@ RSpec.describe 'collections/_collection', type: :view do
   it 'renders a heading link to view the collection' do
     render
     expect(rendered).to have_css('h2 a', text: collection.name)
+  end
+
+  it 'renders a paragraph with the description of the collection' do
+    render
+    expect(rendered).to have_css('p', text: collection.description)
   end
 
   it 'renders the number of workspaces assocated with the collection' do
