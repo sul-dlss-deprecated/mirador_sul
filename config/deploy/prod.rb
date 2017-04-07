4f4dfa112a3cefb8a3cfabe82a1ec4648655e482
Jack Reed <phillipjreed@gmail.com>
2017-04-07 08:23:26 -0400

    use fqdn for hosts

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,4 @@
-server "sul-mirador-prod", user: "mirador", roles: %w{app db web}
+server "sul-mirador-prod.stanford.edu", user: "mirador", roles: %w{app db web}
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :rails_env, 'production'
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,4 +1,4 @@
-server "sul-mirador-stage", user: "mirador", roles: %w{app db web}
+server "sul-mirador-stage.stanford.edu", user: "mirador", roles: %w{app db web}
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :rails_env, 'production'
