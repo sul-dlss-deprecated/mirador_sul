ee4b86655a71dadc477ab9397976124f03e720fc
Javier de la Rosa <versae@gmail.com>
2017-04-07 08:44:46 -0700

    Improving tests and removing not needed line in controller

--- spec/views/collections/edit.html.erb_spec.rb
+++ spec/views/collections/edit.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'collections/edit', type: :view do
   before(:each) do
-    @collection = assign(:collection, create(:collection, workspaces: [create(:workspace)]))
+    @collection = assign(:collection, create(:collection, workspaces: [create(:workspace, name: 'my workspace')]))
   end
 
   it 'renders the edit collection form' do
@@ -11,8 +11,9 @@ RSpec.describe 'collections/edit', type: :view do
     expect(rendered).to have_css 'input[type="submit"]'
   end
 
-  it 'renders the related manifest list' do
+  it 'renders the related workspace list' do
     render
     expect(rendered).to have_css 'h4', text: 'Workspaces'
+    expect(rendered).to have_css 'li', text: 'my workspace'
   end
 end
