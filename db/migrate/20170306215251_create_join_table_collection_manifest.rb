52086134aa4d43b0fcbbde59e30da1a6c1ddfb46
Jessie Keck <jessie.keck@gmail.com>
2021-01-05 15:02:43 -0800

    Tweak old migration to work under modern rails versions.

--- db/migrate/20170306215251_create_join_table_collection_manifest.rb
+++ db/migrate/20170306215251_create_join_table_collection_manifest.rb
@@ -1,9 +1,5 @@
 class CreateJoinTableCollectionManifest < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :collections, :manifests do |t|
-      t.references :collection, index: true, foreign_key: true
-      t.references :manifest, index: true, foreign_key: true
-      t.index [:collection_id, :manifest_id]
-    end
+    create_join_table :collections, :manifests, column_options: { foreign_key: true, index: true }
   end
 end
